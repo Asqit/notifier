28c5012a7b7027a33652132be87990b52bf482e8
Ondřej Tuček <ondrejtucek9@gmail.com>
2024-03-13 12:52:19 +0100

    Improving FE
    - changed to UnoCSS

--- public/static/components/tabs/Tabs.mjs
+++ public/static/components/tabs/Tabs.mjs
@@ -20,13 +20,20 @@ export function Tabs(props) {
 	});
 
 	return html`
-		<div>
+		<div class="p-2 rounded-md bg-zinc-800 w-fit mx-auto">
 			<!--HEADERS-->
-			<div>
+			<div
+				class="flex items-center justify-start gap-x-2 flex-wrap bg-zinc-700 p-1 outline outline-zinc-600 rounded-md mb-2"
+			>
 				${() =>
 					state.tabs.map(
 						(tab, index) => html`
-							<button @click="${() => (state.selectedTab = index)}">
+							<button
+								class="p-1 border-none cursor-pointer text-white ${state.selectedTab === index
+									? "bg-zinc-500"
+									: "bg-zinc-600"}"
+								@click="${() => (state.selectedTab = index)}"
+							>
 								<p>${tab.title}</p>
 							</button>
 						`
@@ -34,7 +41,9 @@ export function Tabs(props) {
 			</div>
 
 			<!--CONTENT-->
-			<div>${() => state.tabs[state.selectedTab].body}</div>
+			<div class="bg-zinc-700 outline outline-zinc-600 mt-4 rounded-md p-2">
+				${() => state.tabs[state.selectedTab].body}
+			</div>
 		</div>
 	`;
 }
